326af96b5e4368f037ea7d5b28597bb36644e258
adam king <kingab_93@yahoo.com>
2021-01-27 22:14:49 -0600

    Cleaned data and filled db tables

--- back-end/beer-schema.sql
+++ back-end/beer-schema.sql
@@ -1,13 +1,19 @@
-CREATE TABLE beers (
+\c capstone2
+
+
+
+
+CREATE TABLE breweries (
     id serial PRIMARY KEY, 
-    brewery_id INT NOT NULL REFERENCES breweries ON DELETE CASCADE,
-    name TEXT NOT NULL, 
-    cat_id INT NOT NULL REFERENCES categories ON DELETE CASCADE,
-    style_id INT NOT NULL REFERENCES styles ON DELETE CASCADE,
-    abv FLOAT,
-    ibu INT,
-    srm INT,
-    upc INT,
+    name TEXT NOT NULL,
+    address1 TEXT, 
+    address2 TEXT, 
+    city TEXT,
+    state TEXT, 
+    code TEXT,
+    country TEXT,
+    phone TEXT,
+    website TEXT,
     filepath TEXT,
     descript TEXT,
     last_mod TIMESTAMP
@@ -21,22 +27,6 @@ CREATE TABLE breweries_geocode (
     accuracy TEXT
 );
 
-CREATE TABLE breweries (
-    id serial PRIMARY KEY, 
-    name TEXT NOT NULL,
-    address1 TEXT, 
-    address2 TEXT, 
-    city TEXT,
-    state TEXT, 
-    code TEXT,
-    country TEXT,
-    phone INT,
-    website TEXT,
-    filepath TEXT,
-    descript TEXT,
-    last_mod TIMESTAMP
-);
-
 CREATE TABLE categories (
     id serial PRIMARY KEY,
     cat_name TEXT,
@@ -44,8 +34,23 @@ CREATE TABLE categories (
 );
 
 CREATE TABLE styles (
-    id serial PRIMARY,
+    id serial PRIMARY KEY,
     cat_id INT REFERENCES categories ON DELETE CASCADE,
     style_name TEXT,
+    last_mod TIMESTAMP
+);
+
+CREATE TABLE beers (
+    id serial PRIMARY KEY, 
+    brewery_id INT NOT NULL REFERENCES breweries ON DELETE CASCADE,
+    name TEXT NOT NULL, 
+    cat_id INT REFERENCES categories ON DELETE CASCADE,
+    style_id INT REFERENCES styles ON DELETE CASCADE,
+    abv FLOAT,
+    ibu TEXT,
+    srm TEXT,
+    upc TEXT,
+    filepath TEXT,
+    descript TEXT,
     last_mod TIMESTAMP
 );
